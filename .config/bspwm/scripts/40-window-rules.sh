0c05b84dee88ad4eb5c8b36a144103a28a61a9c5
Mark Givens <marcxjo@cerebralbleach.com>
2024-06-09 00:22:18 -0600

    bspwm: mark mpv floating/sticky

--- .config/bspwm/scripts/40-window-rules.sh
+++ .config/bspwm/scripts/40-window-rules.sh
@@ -2,6 +2,6 @@
 
 bspc rule -a Gimp     state=floating
 # Media player gets the whole laptop screen to itself
-bspc rule -a mpv      monitor=eDP-1 focus=off
+bspc rule -a mpv      monitor=DP-1 focus=off state=floating sticky=on
 bspc rule -a Surf     state=tiled
 
