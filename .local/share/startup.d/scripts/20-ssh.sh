000815f173c95969321cbd85e178137c4ca09e14
Mark Givens <marcxjo@cerebralbleach.com>
2024-08-16 22:54:04 -0600

    Run LSP squeegee over startup scripts
    There are yet a few more fixes to be made here; mostly low-priority
    fixes that involve refactoring array splits or splitting out
    declarations and definitions

--- .local/share/startup.d/scripts/20-ssh.sh
+++ .local/share/startup.d/scripts/20-ssh.sh
@@ -1,74 +1,70 @@
-#!/bin/sh
+#!/bin/bash
 
 KEY_DIR=${KEY_DIR:-${HOME}/.ssh}
 
 # Functions ####################################################################
 
-function get_key_domain () {
+get_key_domain() {
   # Get the key's associated host/alias and username as a 2-array
   local keyname=${1}
 
-  echo ${keyname} | sed 's|__| |'
+  echo "${keyname//__/ }"
 }
 
-function ls_ssh_keys () {
+ls_ssh_keys() {
   # List all key basenames (i.e., non-public keynames)
   local dir=${1}
 
   # List all the public keys since we can key off of their filenames to
   # locate each respective private key
-  ls ${dir} | grep "\.pub$" | sed 's|\.pub$||'
+  ls "${dir}" | grep "\.pub$" | sed 's|\.pub$||'
 }
 
-function get_ssh_key_fingerprint () {
+get_ssh_key_fingerprint() {
   # Get an SSH key's fingerprint - must pass the full path string
   keypath=${1}
 
-  ssh-keygen -lf ${keypath} | awk '{print $2}'
+  ssh-keygen -lf "${keypath}" | awk '{print $2}'
 }
 
-function check_key_loaded () {
+check_key_loaded() {
   # Check by fingerprint whether a key is already registered with the SSH agent
   local fingerprint=${1}
 
-  ssh-add -l | grep -q ${fingerprint}
+  ssh-add -l | grep -q "${fingerprint}"
 }
 
 # Startup ######################################################################
 
-if [ ! -S ${HOME}/.ssh/ssh_auth_sock ]; then
-  eval $(ssh-agent -s)
-  ln -sf "$SSH_AUTH_SOCK" ${HOME}/.ssh/ssh_auth_sock
+if [ ! -S "${HOME}/.ssh/ssh_auth_sock" ]; then
+  eval "$(ssh-agent -s)"
+  ln -sf "$SSH_AUTH_SOCK" "${HOME}/.ssh/ssh_auth_sock"
 fi
 
 export SSH_AUTH_SOCK=${HOME}/.ssh/ssh_auth_sock
 
 # Key Registration #############################################################
 
-if ! command -v pw &> /dev/null
-then
+if ! command -v pw &>/dev/null; then
   echo "pw is not installed"
   echo "Can not register ssh-keys"
-  exit 1
+  return 1
 fi
 
-for key in $(ls_ssh_keys ${KEY_DIR})
-do
-  domain_components=( $(get_key_domain $key) )
-  fingerprint=$(get_ssh_key_fingerprint ${KEY_DIR}/${key})
+for key in $(ls_ssh_keys "${KEY_DIR}"); do
+  domain_components=($(get_key_domain $key))
+  fingerprint=$(get_ssh_key_fingerprint "${KEY_DIR}/${key}")
 
-  if check_key_loaded ${fingerprint}
-  then
+  if check_key_loaded "${fingerprint}"; then
     continue
   fi
 
   SSH_HOST=${domain_components[0]} \
-  SSH_USER=${domain_components[1]} \
-  SSH_ASKPASS_REQUIRE=force \
-  SSH_ASKPASS=$(command -v ssh-pass.sh) \
-  ssh-add ${KEY_DIR}/${key} < /dev/null
+    SSH_USER=${domain_components[1]} \
+    SSH_ASKPASS_REQUIRE=force \
+    SSH_ASKPASS=$(command -v ssh-pass.sh) \
+    ssh-add "${KEY_DIR}/${key}" </dev/null
 done
 
 unset SSH_HOST
 unset SSH_USER
-
