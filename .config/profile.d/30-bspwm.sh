99f9d91da84b018bce36148e1ec76e789ca066fe
Mark Givens <marcxjo@cerebralbleach.com>
2024-12-27 09:18:27 -0700

    profile.d: drop stulto support; unify media player envars

--- .config/profile.d/30-bspwm.sh
+++ .config/profile.d/30-bspwm.sh
@@ -6,7 +6,4 @@ if [[ "${XDG_SESSION_DESKTOP,,}" != "bspwm" ]]; then
   return
 fi
 
-export STULTO_HEADERBAR_TYPE=2
-export LETS_WATCH_MEDIA_PLAYER='umpv'
-export YOUTUBE_PL_MEDIA_PLAYER='umpv'
-export YT_MEDIA_PLAYER='umpv'
+export MEDIA_PLAYER='umpv'
--- .config/profile.d/30-gnome.sh
+++ .config/profile.d/30-gnome.sh
@@ -6,7 +6,4 @@ if [[ "${XDG_CURRENT_DESKTOP,,}" != "gnome" ]]; then
   return
 fi
 
-export STULTO_HEADERBAR_TYPE=0
-export LETS_WATCH_MEDIA_PLAYER='celluloid --enqueue'
-export YOUTUBE_PL_MEDIA_PLAYER='celluloid --enqueue'
-export YT_MEDIA_PLAYER='celluloid --enqueue'
+export MEDIA_PLAYER='celluloid'
