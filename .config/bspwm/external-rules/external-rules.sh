0969186ff20c20557474c9858eba3df8b9f2d975
Mark Givens <marcxjo@cerebralbleach.com>
2024-12-27 09:12:43 -0700

    [WIP] bspwm: refactor external rule handling

--- .config/bspwm/external-rules/external-rules.sh
+++ .config/bspwm/external-rules/external-rules.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
+get_float_area() {
+  local -r _mon="$1"
+
+  local -r area_width=$(bspc config -m $mon left_padding)
+}
+
 declare -r window_id="$1"
 declare -r window_class="$2"
 declare -r window_instance="$3"
 
-declare -r window_role="$(xprop -id "$window_id" WM_WINDOW_ROLE | sed '/^WM_WINDOW_ROLE/!d ; s/^.* = "\(.*\)"/\1/')"
-
 declare -i WINDOW_PLACEMENT_W WINDOW_PLACEMENT_H WINDOW_PLACEMENT_X WINDOW_PLACEMENT_Y
 declare WINDOW_NAME
 
-# if [[ " ${window_class,,} " =~ " mpv " ]]; then
-#   _rectangle=$(${HOME}/.config/bspwm/external-rules/calculate-placement.sh 'mpv')
-#   echo "state=floating sticky=on focus=off rectangle=${_rectangle}"
-#   exit 0
-# fi
-#
-# if [[ " ${window_role,,} " =~ " omnibox " ]]; then
-#   _rectangle=$(${HOME}/.config/bspwm/external-rules/calculate-placement.sh 'omnibox')
-#   echo "state=floating sticky=on rectangle=${_rectangle}"
-#   exit 0
-# fi
+declare -rA RULES=(
+#["mpv"]="state=floating sticky=on focus=off rectangle=780x624+220+80"
+["stulto"]="state=floating sticky=on rectangle=820x660+35+750"
+)
 
-if [[ " ${window_class,,} " =~ " firefox " ]]; then
-  _rectangle=$(${HOME}/.config/bspwm/external-rules/calculate-placement.sh 'firefox')
-  echo "state=floating sticky=on focus=off rectangle=${_rectangle}"
+if [[ "x${RULES[${window_class}],,}" == "x" ]]; then
   exit 0
 fi
+
+echo "${RULES[${window_class}]}"
