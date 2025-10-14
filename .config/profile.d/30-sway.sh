5858ce40edf7db072b8095c19336aff04b5684a3
Marko Givens <marcxjo@cerebralbleach.com>
2025-10-14 11:28:57 -0600

    sway: fix up screen background handling; clean up some minor cruft

--- .config/profile.d/30-sway.sh
+++ .config/profile.d/30-sway.sh
@@ -6,4 +6,12 @@ if [[ ! ":${XDG_CURRENT_DESKTOP,,}:" =~ ":sway:" ]]; then
   return
 fi
 
+SWAY_SCRIPTS_DIR="${HOME}/.config/sway/scripts"
+
+[[ ! ":${PATH}:" =~ ":${SWAY_SCRIPTS_DIR}:" ]] &&
+  [[ -d "${SWAY_SCRIPTS_DIR}" ]] &&
+  export PATH="${PATH}:${SWAY_SCRIPTS_DIR}"
+
+unset SWAY_SCRIPTS_DIR
+
 export MEDIA_PLAYER='umpv'
