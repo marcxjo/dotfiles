98f981a847a8489bbee33223bed0835cc14668b2
Marko Givens <marcxjo@cerebralbleach.com>
2026-03-20 22:10:05 -0600

    profile.d: start Void Linux services unless logged in via ssh

--- .config/profile.d/10-voidlinux.sh
+++ .config/profile.d/10-voidlinux.sh
@@ -2,9 +2,22 @@
 # Void Linux-specific bits
 #
 
-if [ "$(lsb_release -d | awk '{$1=""; gsub(/^ */, "", $0); print}')" == "Void Linux" ]; then
+if [ ! "$(lsb_release -d | awk '{$1=""; gsub(/^ */, "", $0); print}')" == "Void Linux" ]; then
   return
 fi
 
-# Void Linux user services directory
-export SVDIR="${HOME}/.local/share/runit/runsvdir"
+# We don't need session services if we're just SSHing in
+# I might eventually wire up support for SSH-specific services if a compelling
+# need arises
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+  return
+fi
+
+# Enable running user-specific services when running `runit` commands
+# unprivileged
+export SVDIR="${HOME}/.local/state/service"
+export RUNSVDIR="${HOME}/.local/share/runit/runsvdir"
+
+mkdir -p "/run/user/${UID}/runit"
+
+runsvdir -P "$SVDIR" &
